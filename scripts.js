db6c669154dded5732e2dd3b3201650e7cb0f17d
qba <qbasa@wp.pl>
2017-04-26 13:49:46 +0200

    kolejne zadanie

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var a = prompt("Podaj wartość a"),
+	b = prompt("Podaj wartość b"),
+	value = (a * a) + (2 * a * b) - (b * b),
+	wynik = "";
+if( value > 0) {
+	wynik = "Działanie ma wartość dodatnią i wynosi: " +value;
+}
+else {
+	wynik = "Działanie ma wartość ujemną i wynosi: " +value;
+}
+console.log(wynik);
+document.getElementById("wynik").innerHTML = wynik;
