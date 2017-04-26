314e51b9a4289eca61455ccdeecc74c3e8aab423
qba <qbasa@wp.pl>
2017-04-26 13:55:28 +0200

    Dodanie if które sprawdza czy wartość jest równa  0

--- scripts.js
+++ scripts.js
@@ -2,11 +2,18 @@ var a = prompt("Podaj wartość a"),
 	b = prompt("Podaj wartość b"),
 	value = (a * a) + (2 * a * b) - (b * b),
 	wynik = "";
+
 if( value > 0 ) {
 	wynik = "Działanie ma wartość dodatnią i wynosi: " +value;
 }
+
+else if ( value == 0 ) {
+	wynik = "Działanie ma wartość zerową";
+}
+
 else {
 	wynik = "Działanie ma wartość ujemną i wynosi: " +value;
 }
+
 console.log(wynik);
 document.getElementById("wynik").innerHTML = wynik;
