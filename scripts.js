7ca103a78c1bdf6164e630daa7d2aebe240fb0bf
qba <qbasa@wp.pl>
2017-04-26 13:50:58 +0200

    fix

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var a = prompt("Podaj wartość a"),
 	b = prompt("Podaj wartość b"),
 	value = (a * a) + (2 * a * b) - (b * b),
 	wynik = "";
-if( value > 0) {
+if( value > 0 ) {
 	wynik = "Działanie ma wartość dodatnią i wynosi: " +value;
 }
 else {
